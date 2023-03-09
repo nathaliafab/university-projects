20fdaafeaf0f4e852f7d8abef0cb5163aa064cb5
Nathalia Barbosa <nathaliaferdab@gmail.com>
2023-03-09 11:17:23 -0300

    add exemplos de integração c-assembly

---
+++ exercícios/C-asm/imprimir/printc.c
@@ -0,0 +1,3 @@
+void imprimir(double valor) {
+    printf("%lf", valor);
+}
---
+++ exercícios/C-asm/somar/somac.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+extern int soma(int, int, int);
+
+int main(void)
+{
+    int a = 1, b = 4, c = 5, value;
+    value = soma(a, b, c);
+}
