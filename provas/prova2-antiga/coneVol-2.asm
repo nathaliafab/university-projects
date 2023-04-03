85c844679d47b35a33123385ef2a49c251523a53
Nathalia Barbosa <nathaliaferdab@gmail.com>
2023-04-03 15:43:42 -0300

    conserto de erros

--- provas/prova2-antiga/coneVol-2.asm
+++ provas/prova2-antiga/coneVol-2.asm
@@ -1,21 +1,20 @@
 ;Complete o código assembly abaixo utilizando instruções de ponto flutuante para
 ;implementar o cálculo do volume de um cone (PI * altura * raio²/3)
 
-SECTION .data
-
-SECTION .text
-
 global calcVol
+extern scanf
 
 calcVol:
     finit
     enter 0,0
-    fld dword[ebp+8]    ;empilha raio
-    fld dword[ebp+8]    ;empilha raio
+
+    mov eax, [ebp+8]    ;guarda o endereço de volume em eax
+    fld dword[ebp+12]   ;empilha raio
+    fld dword[ebp+12]   ;empilha raio
     fmulp               ;raio*raio
     fldpi               ;empilha pi
     fmulp               ;pi*raio*raio
-    fld dword[ebp+12]   ;empilha altura
+    fld dword[ebp+16]   ;empilha altura
     fmulp               ;altura*pi*raio*raio
     fld1                ;empilha 3 (1+1+1)
     fld1
@@ -23,6 +22,6 @@ calcVol:
     fld1
     faddp
     fdivp               ;divide (altura*pi*raio*raio)/3
-    fst dword[ebp+16]   ;armazena o resultado em volume
+    fstp dword[eax]     ;armazena o resultado em volume
     leave
     ret
